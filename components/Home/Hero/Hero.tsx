1f1afe299606ab5a5a60bb20615f0c558ce44a34
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-14 05:58:29 +0100

    📈 working on the Hero section  📈

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative w-full h-screen flex justify-center flex-col'>
+        <div className='w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-10'>
+            {/* Text content */}
+            <div></div>
+            {/* Image content */}
+            <div></div>
+        </div>
+    </div>
   )
 }
 
